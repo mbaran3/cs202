1a84278a71378ac671794d91b271c4d661d262e0
Michael baran <michaelb.baran14712@gmail.com>
2021-01-22 05:09:37 -0900

    added no valid input error message

--- HW0/HW0/Temperature.cpp
+++ HW0/HW0/Temperature.cpp
@@ -12,12 +12,6 @@ double c_ctof(const char* str) {
 	double temp = std::strtod(str, NULL);
 	return (temp * 9.0 / 5.0) + 32;
 }
-double ftoc(double temp) {
-	return (5.0 / 9.0) * (temp - 32);
-}
-double ctof(double temp) {
-	return (temp * 9.0 / 5.0) + 32;
-}
 
 int main(int argc, char* argv[]) {
 	
@@ -26,21 +20,18 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-
-	
-	
-
 	if (strcmp(argv[1], "ftoc") == 0) {
 
 		try {
 			double tester = std::stod(argv[2]);
 		}
 		catch (const std::invalid_argument&) {
-			std::cout << "Error: '" << argv[2] << "' is an invalid arguement\n";
+			std::cout << "ERROR: '" << argv[2] << "' is an invalid arguement\n";
 			return 0;
 		}
 
 		std::cout << argv[2] << " degrees Fahrenheit equals " << cpp_ftoc(argv[2]) << " degrees Celsius\n";
+		return 0;
 	}
 		
 	if (strcmp(argv[1], "ctof") == 0) {
@@ -52,22 +43,8 @@ int main(int argc, char* argv[]) {
 		}
 			
 		std::cout << argv[2] << " degrees Celsius equals " << c_ctof(argv[2]) << " degress Fahrenheit\n";
-	}
-		
-
-	return 0;
-}
-
-
-	//Converts ctof and ftoc using ftoc and ctof 
-	/*if (argc < 3) {
-		std::cout << "Few Arguements\n";
 		return 0;
 	}
-	double start_deg = atof(argv[2]);
-
-	if (strcmp(argv[1], "ftoc") == 0)
-		std::cout << start_deg << " degrees Fahrenheit equals " << ftoc(start_deg) << " degrees Celsius\n";
-	if (strcmp(argv[1], "ctof") == 0)
-		std::cout << start_deg << " degrees Celsius equals" << ctof(start_deg) << " degress Fahrenheit\n";
-	*/
+	std::cout << "ERROR: No Valid input"
+	return 0;
+}
