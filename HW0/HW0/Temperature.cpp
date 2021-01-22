7046d69bc326eed5b693916c75de60a198b0e6a5
Michael baran <michaelb.baran14712@gmail.com>
2021-01-22 04:28:06 -0900

    Fix the ability for a user to enter a non number as the second arguement. Used a try-block for stod, and a endpoint  to test strod.

--- HW0/HW0/Temperature.cpp
+++ HW0/HW0/Temperature.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 
 double cpp_ftoc(const char* str) {
-	double temp  = std::stod(str);
-	return (5.0 / 9.0) * (temp - 32);
+	double  temp = std::stod(str);
+	double converted_temp = (5.0 / 9.0) * (temp - 32);
+	return converted_temp;
 }
 double c_ctof(const char* str) {
 
@@ -21,14 +22,39 @@ double ctof(double temp) {
 int main(int argc, char* argv[]) {
 	
 	if (argc < 3 || argc > 3) {
-		std::cout << "Error\n";
+		std::cout << "Error to many arguements\n";
 		return 0;
 	}
-	if (strcmp(argv[1], "ftoc") == 0)
-		std::cout << argv[2] << " degrees Fahrenheit equals " << cpp_ftoc(argv[2]) << " degrees Celsius\n";
-	if (strcmp(argv[1], "ctof") == 0)
-		std::cout << argv[2] << " degrees Celsius equals " << c_ctof(argv[2]) << " degress Fahrenheit\n";
+
+
 	
+	
+
+	if (strcmp(argv[1], "ftoc") == 0) {
+
+		try {
+			double tester = std::stod(argv[2]);
+		}
+		catch (const std::invalid_argument&) {
+			std::cout << "Error: '" << argv[2] << "' is an invalid arguement\n";
+			return 0;
+		}
+
+		std::cout << argv[2] << " degrees Fahrenheit equals " << cpp_ftoc(argv[2]) << " degrees Celsius\n";
+	}
+		
+	if (strcmp(argv[1], "ctof") == 0) {
+		char* endprt;
+		double tester = std::strtod(argv[2], &endprt);
+		if (argv[2] == endprt) {
+			std::cout << "ERROR: '" << argv[2] << "' is an invalid arguement\n";
+			return 0;
+		}
+			
+		std::cout << argv[2] << " degrees Celsius equals " << c_ctof(argv[2]) << " degress Fahrenheit\n";
+	}
+		
+
 	return 0;
 }
 
