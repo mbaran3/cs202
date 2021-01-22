567ea67f1ed35e111f7d0b1bad0023f6fb918f20
Michael baran <michaelb.baran14712@gmail.com>
2021-01-22 03:38:11 -0900

    completed cpp_ftoc, and c_ctof

--- HW0/HW0/Temperature.cpp
+++ HW0/HW0/Temperature.cpp
@@ -3,13 +3,13 @@
 #include <vector>
 
 double cpp_ftoc(const char* str) {
-		
-	std::cout << str;
-	return 0;
+	double temp  = std::stod(str);
+	return (5.0 / 9.0) * (temp - 32);
 }
 double c_ctof(const char* str) {
 
-	return 0;
+	double temp = std::strtod(str, NULL);
+	return (temp * 9.0 / 5.0) + 32;
 }
 double ftoc(double temp) {
 	return (5.0 / 9.0) * (temp - 32);
@@ -20,8 +20,21 @@ double ctof(double temp) {
 
 int main(int argc, char* argv[]) {
 	
+	if (argc < 3 || argc > 3) {
+		std::cout << "Error\n";
+		return 0;
+	}
+	if (strcmp(argv[1], "ftoc") == 0)
+		std::cout << argv[2] << " degrees Fahrenheit equals " << cpp_ftoc(argv[2]) << " degrees Celsius\n";
+	if (strcmp(argv[1], "ctof") == 0)
+		std::cout << argv[2] << " degrees Celsius equals " << c_ctof(argv[2]) << " degress Fahrenheit\n";
 	
-	if (argc < 3) {
+	return 0;
+}
+
+
+	//Converts ctof and ftoc using ftoc and ctof 
+	/*if (argc < 3) {
 		std::cout << "Few Arguements\n";
 		return 0;
 	}
@@ -31,6 +44,4 @@ int main(int argc, char* argv[]) {
 		std::cout << start_deg << " degrees Fahrenheit equals " << ftoc(start_deg) << " degrees Celsius\n";
 	if (strcmp(argv[1], "ctof") == 0)
 		std::cout << start_deg << " degrees Celsius equals" << ctof(start_deg) << " degress Fahrenheit\n";
-	
-	return 0;
-}
+	*/
