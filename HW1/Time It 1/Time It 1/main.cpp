651211c058674f2d4e4430b0ab35f17d4dc798a2
Michael baran <michaelb.baran14712@gmail.com>
2021-01-28 15:26:26 -0900

    Created generateVector() function to create vectors

--- HW1/Time It 1/Time It 1/main.cpp
+++ HW1/Time It 1/Time It 1/main.cpp
@@ -5,19 +5,30 @@
 #include "StopWatch.h";
 
 
-void generateVector(int size) {
+std::vector<int> generateVector(int size) {
 
+	std::random_device seed;
+	std::mt19937 generator(seed());
+	std::uniform_int_distribution<int> randNum(0, size);
+	StopWatch Time;
+	std::vector<int> vInt(size);
+	for (int i = 0; i < size; i++)
+		vInt[i] = randNum(generator);
+	std::cout << size << " Vector took " << Time.getDuration() << "s to generate" << std::endl;
+	return vInt;
 
+}
+void binarySearch(std::vector<int> vInt, int needle) {
+	
+	StopWatch Time;
+	std::binary_search(vInt.begin(), vInt.end(), needle);
+	std::cout << "Search took " << Time.getDuration() << "s" << std::endl;
 
 }
 
 int main() {
 
-	StopWatch Time;
-	std::vector<int> vInt;
-	for (int i = 0; i < 100000; i++)
-		vInt.push_back(i);
-	std::cout << Time.getDuration() << "s\n";
+	
 
 
 	return 0;
