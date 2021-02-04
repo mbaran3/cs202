16abfddf513bc15875cb976005981cd9b28e3b5a
Michael baran <michaelb.baran14712@gmail.com>
2021-02-04 06:57:02 -0900

    Created and tested loadFiletoList();

--- HW1/Time It 2/Time It 2/main.cpp
+++ HW1/Time It 2/Time It 2/main.cpp
@@ -1,12 +1,24 @@
 #include "StopWatch.h"
 #include <vector>
+#include <list>
 #include <string>
 #include <fstream>
 #include <sstream>
 
 
 
+std::list<std::string> loadFiletoList(std::string fileName) {
 
+	StopWatch Time;
+	std::list<std::string> listOutput;
+	std::string output;
+	std::ifstream inFile(fileName);
+	while (std::getline(inFile, output))
+		listOutput.push_back(output);
+	std::cout << fileName << " Took " << Time.getDurationmilli() << "ms to load to a list" << std::endl;
+	return listOutput;
+
+}
 std::vector<std::string> loadFiletoVector(std::string fileName) {
 
 	StopWatch Time;
@@ -23,6 +35,8 @@ int main() {
 
 
 	std::vector<std::string> vTaleoftwoCities = loadFiletoVector("TaleoftwoCities.txt");
+	std::list<std::string> listTaleoftwoCities = loadFiletoList("Taleoftwocities.txt");
+
 
 	return 0;
 }
