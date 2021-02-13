ede0039440dbd5c0a907e2af3dbb126cee796391
Michael baran <michaelb.baran14712@gmail.com>
2021-02-12 16:55:03 -0900

    Wrote and tested to_lower()

--- HW2/Lower Case Letters/Lower Case Letters/main.cpp
+++ HW2/Lower Case Letters/Lower Case Letters/main.cpp
@@ -1,8 +1,24 @@
-#include <iosteam>
+#include <iostream>
 
+void to_lower(char* string) {
 
+	int size = 0;
+	while (string[size]) // Gets the size of the array
+		size++;
+	for (int i = 0; i < size; i++) // Goes through the array
+		for (char j = 65; j <= 90; j++) //Goes through the alphabet using ASCII
+			if (j == string[i])
+				string[i] += 32; // adds 32 to the character changing it for uppercase to lowercase
+}
 int main() {
 
+	char str[] = "HELLO WORLD!";
+	
+
+	std::cout << "Before to_lower is called : " << str << std::endl;
+	to_lower(str);
+	std::cout<< "After to_lower is called : " << str << std::endl;
+	
 
 
 	return 0;
