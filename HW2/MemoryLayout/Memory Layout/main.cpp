45b132b28d048191402acaae99792cfa10d90fc4
Michael baran <michaelb.baran14712@gmail.com>
2021-02-12 16:05:26 -0900

    Created if statements to see which stored higher in memory (Free storage > static data > the stack)

--- HW2/MemoryLayout/Memory Layout/main.cpp
+++ HW2/MemoryLayout/Memory Layout/main.cpp
@@ -10,6 +10,28 @@ int main() {
 	std::cout << "static data location :" << &_static << std::endl;
 	std::cout << "free storage location :" << &free << std::endl;
 	std::cout << "Stack storage location :" << &stack << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "Static vs Free" << std::endl;
+	if (&_static > free)
+		std::cout << "Static Data is stored higher in memory" << std::endl;
+	else
+		std::cout << "Free storage is stored higher in memory" << std::endl;
+
+
+	std::cout << "Free vs Stack" << std::endl;
+	if (&stack > free)
+		std::cout << "Stack data is stored higher in memory" << std::endl;
+	else
+		std::cout << "Free storage is stored higher in memory" << std::endl;
+
+
+	std::cout << "Static vs Stack" << std::endl;
+	if (&stack > &_static)
+		std::cout << "Static Data is stored higher in memory" << std::endl;
+	else
+		std::cout << "Stack data is stored higher in memory" << std::endl;
 	
 	return 0;
 }
