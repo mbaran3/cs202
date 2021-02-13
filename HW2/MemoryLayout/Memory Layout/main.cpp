6f27f5a1e8d0ea9af26a240e77c4a56278be5d49
Michael baran <michaelb.baran14712@gmail.com>
2021-02-12 16:12:17 -0900

    Created a way to see which way the stack grows and tests(the stack grows up). Created a way to  see which way the heap grows(down)

--- HW2/MemoryLayout/Memory Layout/main.cpp
+++ HW2/MemoryLayout/Memory Layout/main.cpp
@@ -4,8 +4,11 @@ static int _static;
 int main() {
 
 	int* free = new int;
-	int stack = 0;
+	int* free2 = new int;
+	int* array = new int[100];
 
+	int stack = 0;
+	int stack2 = 0;
 
 	std::cout << "static data location :" << &_static << std::endl;
 	std::cout << "free storage location :" << &free << std::endl;
@@ -33,5 +36,25 @@ int main() {
 	else
 		std::cout << "Stack data is stored higher in memory" << std::endl;
 	
+	std::cout << std::endl;
+
+	if (&stack > &stack2)
+		std::cout << "The stack grows downwards towards lower addresses in memory" << std::endl;
+	else
+		std::cout << "The stack grows upwards towards higher addresses in memory" << std::endl;
+
+	std::cout << std::endl;
+
+	if (free > free2)
+		std::cout << "Free memory grows downwards towards lower addresses in memory" << std::endl;
+	else
+		std::cout << "Free memory grows upwards towards higher address in memory" << std::endl;
+
+	if (array[0] > array[99])
+		std::cout << "Elements with higher indices allocate to lower addresses" << std::endl;
+	else
+		std::cout << "Elemets with higher indices allocate to higher addresses" << std::endl;
+
+
 	return 0;
 }
