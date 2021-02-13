a9fc742b2ba42362809feed3ba1ccd2aeecefb15
Michael baran <michaelb.baran14712@gmail.com>
2021-02-12 16:02:05 -0900

    wrote code to cout the location of a global static variable(static data), a variable pointer(free memory), and a variable(the stack).

--- HW2/MemoryLayout/Memory Layout/main.cpp
+++ HW2/MemoryLayout/Memory Layout/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-
+static int _static;
 int main() {
+
+	int* free = new int;
+	int stack = 0;
+
+
+	std::cout << "static data location :" << &_static << std::endl;
+	std::cout << "free storage location :" << &free << std::endl;
+	std::cout << "Stack storage location :" << &stack << std::endl;
 	
 	return 0;
 }
