53b51c7ba73c1148f09a56c536a34a15e1cd5ab4
Michael baran <michaelb.baran14712@gmail.com>
2021-02-25 10:45:54 -0900

    created and tested lineToTokens

--- HW3/A Simple Tokenizer/main.cpp
+++ HW3/A Simple Tokenizer/main.cpp
@@ -4,17 +4,40 @@
 #include <sstream>
 #include <fstream>
 
+
+struct TokenAndPosition {
+
+	std::string token;
+	int line;
+	int column;
+};
+
+std::vector<std::string> lineToTokens(const std::string& line) {
+
+	std::string token;
+	std::vector<std::string> vToken;
+	std::stringstream ss(line);
+	while (ss >> token) 
+		vToken.push_back(token);
+	return vToken;
+
+}
+
 int main() {
 
 	std::string fileName = "testing.txt";
 	std::fstream inFile(fileName);
+	std::vector<std::string> vTest = lineToTokens("Hello this is a test hi");
+	
+	for (auto i : vTest)
+		std::cout << i << " ";
+	std::cout << vTest.size();
 
+	std::cout << std::endl;
+	
 	if (!inFile)
-		std::cout << "Error reading file" std::endl;
+		std::cout << "Error reading file" << std::endl;
 	else
 		std::cout << "Working" << std::endl;
-
-
-
 
 }
