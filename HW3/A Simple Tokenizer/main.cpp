235f34e4ce58e3457d95daef39ad2e70d0735af6
Michael baran <michaelb.baran14712@gmail.com>
2021-02-25 11:08:15 -0900

    created and tested readLines()

--- HW3/A Simple Tokenizer/main.cpp
+++ HW3/A Simple Tokenizer/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 #include <sstream>
 #include <fstream>
 
@@ -22,22 +23,45 @@ std::vector<std::string> lineToTokens(const std::string& line) {
 	return vToken;
 
 }
+std::vector<TokenAndPosition> readLines(std::fstream& is) {
+
+	std::string line;
+	std::vector<TokenAndPosition> tokenInfo;
+	int lineCounter = 1;
+	while (std::getline(is, line)) {
+
+		std::vector<std::string> tokens = lineToTokens(line);
+		int column = 0;
+		for (auto i : tokens) {
+			TokenAndPosition singleToken{ i, lineCounter, column };
+			tokenInfo.push_back(singleToken);
+			column += i.size()+1;
+		}
+		++lineCounter;
+	}
+	
+	return tokenInfo;
+}
 
 int main() {
 
 	std::string fileName = "testing.txt";
 	std::fstream inFile(fileName);
-	std::vector<std::string> vTest = lineToTokens("Hello this is a test hi");
-	
-	for (auto i : vTest)
-		std::cout << i << " ";
-	std::cout << vTest.size();
 
-	std::cout << std::endl;
-	
 	if (!inFile)
 		std::cout << "Error reading file" << std::endl;
 	else
 		std::cout << "Working" << std::endl;
+
+	std::vector<TokenAndPosition> tokenized = readLines(inFile);
+	std::cout << std::setw(3) << "Ln "  << "Col " << "Word" << std::endl;
+	for (auto i : tokenized) {
+		std::cout << std::setw(3) << i.line << " ";
+		std::cout << std::setw(3) << i.column << " ";
+		std::cout << i.token << std::endl;
+	}
+
+
+	return 0;
 
 }
